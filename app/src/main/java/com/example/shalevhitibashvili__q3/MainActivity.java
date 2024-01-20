116e4b3d344508e1905fec2729d5e5e175ab354c
shalevhiti1 <tayger110807@gmail.com>
2024-01-20 22:34:45 +0200

    Q3

--- app/src/main/java/com/example/shalevhitibashvili__q3/MainActivity.java
+++ app/src/main/java/com/example/shalevhitibashvili__q3/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.shalevhitibashvili__q3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    int num=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.button);
+    }
+    public void updateText(View v){
+        num++;
+        if (num==7){
+            button.setText("Enough to click. Go to new start!");
+            num=0;
+        }
+        else button.setText("This is a click number:"+"  "+num);
     }
 }
